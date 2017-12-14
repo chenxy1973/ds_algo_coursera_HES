1a37b3097a052464b6bf41554c95b49367716cb2
Chen Xiaoyuan <chenxy@BWAVE.LOCAL>
2017-12-14 18:02:46 +0800

    sorting python program commited, but failed due to time-limit exceeded

--- algorithmic_toolbox/03_divide_and_conquer_starter_files/sorting/sorting.py
+++ algorithmic_toolbox/03_divide_and_conquer_starter_files/sorting/sorting.py
@@ -2,10 +2,6 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
-
 def partition2(a, l, r):
     x = a[l]
     j = l;
@@ -13,9 +9,8 @@ def partition2(a, l, r):
         if a[i] <= x:
             j += 1
             a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
+    a[l], a[j] = a[j], a[l]  # WHY??? chenxy@2017-12-14
     return j
-
 
 def randomized_quick_sort(a, l, r):
     if l >= r:
@@ -27,10 +22,42 @@ def randomized_quick_sort(a, l, r):
     randomized_quick_sort(a, l, m - 1);
     randomized_quick_sort(a, m + 1, r);
 
+def partition3(a, l, r):
+    #write your code here
+    x = a[l]
+    j = l;
+    for i in range(l + 1, r + 1):
+        if a[i] <= x:
+            j += 1
+            a[i], a[j] = a[j], a[i]
+    a[l], a[j] = a[j], a[l]  # WHY??? chenxy@2017-12-14
+    
+    m_right = j
+    
+    #Further, search for m_left
+    for i in range(l,m_right):
+        if a[m_right-1-i] == x:
+            j = j - 1
+            a[m_right-1-i], a[j] = a[j], a[m_right-1-i]
+            
+    m_left = j
+        
+    return m_left, m_right
+
+def randomized_quick_sort3(a, l, r):
+    if l >= r:
+        return
+    k = random.randint(l, r)
+    a[l], a[k] = a[k], a[l]
+    #use partition3
+    m_left,m_right = partition3(a, l, r)
+    randomized_quick_sort3(a, l, m_left - 1);
+    randomized_quick_sort3(a, m_right + 1, r);
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    randomized_quick_sort(a, 0, n - 1)
+    #randomized_quick_sort(a, 0, n - 1)
+    randomized_quick_sort3(a, 0, n - 1)
     for x in a:
         print(x, end=' ')
